b8d1a6b962464ebf944cb82ff814c6191e927403
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-08-21 13:15:43 +0530

    14/08

--- project13/student/models.py
+++ project13/student/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
 # Create your models here.
-class Student(models.Model):
-    name=models.CharField(max_length=20)
-    age=models.IntegerField()
-    city=models.CharField(max_length=50)
-
-    def __str__(self):
+class Student(models.Model): 
+    name = models.CharField(max_length=100) 
+    age = models.IntegerField(default=None) 
+    marks = models.IntegerField(default=None) 
+    city = models.CharField(max_length=50) 
+    course = models.CharField(max_length=50,default=None) 
+    fees = models.IntegerField(default=None) 
+ 
+    def __str__(self): 
         return self.name
-    
+       

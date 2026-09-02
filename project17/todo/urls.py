9b2903b2dd4a688c6f0c48e2084d4b198450fced
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-09-02 13:18:13 +0530

    14/08

--- project17/todo/urls.py
+++ project17/todo/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
-# app_name = 'todo'
+app_name = 'todo'
 
 urlpatterns = [
     path('', views.show_tasks, name='show_tasks'),
+    path('add/', views.add_task, name='add_task'),
+    path('toggle/<int:task_id>/', views.toggle_task, name='toggle_task'),
 ]
--- project17/todo/views.py
+++ project17/todo/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
 from .models import Task
 # Create your views here.
 def show_tasks(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.all().order_by('-created_at')
     return render(request, 'todo/task.html', {'tasks': tasks})
+
+def toggle_task(request, task_id):
+    # task = Task.objects.get(id=task_id)
+    task = get_object_or_404(Task, id=task_id)
+    task.completed = not task.completed
+    task.save()
+    return redirect('todo:show_tasks')
+
+def add_task(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')   
+        if title and description:
+            Task.objects.create(title=title, description=description)
+            return redirect('todo:show_tasks')
+        return render(request, 'todo/add_task.html', {'error': 'Please provide both title and description.'})   
+    return render(request, 'todo/add_task.html')
